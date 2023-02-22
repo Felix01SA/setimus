10c0682ec6a6935a1e048635426d097be3abddb5
Felix01SA <felixsouza1200@gmail.com>
2023-02-22 20:47:17 -0300

    Update maintence functions

--- src/utils/functions/maintenance.ts
+++ src/utils/functions/maintenance.ts
@@ -1,24 +1,31 @@
-import { Data } from "@entities"
-import { Database } from "@services"
-import { resolveDependency } from "@utils/functions"
+import { Data } from '@entities'
+import { Database } from '@services'
+import { resolveDependency } from '@utils/functions'
+import { ModelData } from '@entities'
 
 /**
  * Get the maintenance state of the bot.
  */
 export const isInMaintenance = async (): Promise<boolean> => {
-            
-    const db = await resolveDependency(Database)
-    const dataRepository = db.get(Data)
-    const maintenance = await dataRepository.get('maintenance')
-    
+    const database = new ModelData()
+
+    const maintenance = await database.get('maintenance')
+
+    // const db = await resolveDependency(Database)
+    // const dataRepository = db.get(Data)
+    // const maintenance = await dataRepository.get('maintenance')
+
     return maintenance
 }
 
 /**
  * Set the maintenance state of the bot.
  */
-export const setMaintenance = async (maintenance: boolean) =>  {
-    const db = await resolveDependency(Database)
-    const dataRepository = db.get(Data)
-    await dataRepository.set('maintenance', maintenance)
+export const setMaintenance = async (maintenance: boolean) => {
+    // const db = await resolveDependency(Database)
+    // const dataRepository = db.get(Data)
+    // await dataRepository.set('maintenance', maintenance)
+
+    const database = new ModelData()
+    await database.set('maintenance', maintenance)
 }
