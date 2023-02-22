615afb8e8f6d8a990a6019ce6bc09ccf26acb709
Felix01SA <felixsouza1200@gmail.com>
2023-02-22 20:43:44 -0300

    entities updates

--- src/entities/Data.ts
+++ src/entities/Data.ts
@@ -1,16 +1,21 @@
-import { Entity, EntityRepositoryType, PrimaryKey, Property } from "@mikro-orm/core"
-import { EntityRepository } from "@mikro-orm/sqlite"
+import {
+    Entity,
+    EntityRepositoryType,
+    PrimaryKey,
+    Property,
+} from '@mikro-orm/core'
+import { EntityRepository } from '@mikro-orm/sqlite'
+import { prisma } from '@utils/functions'
 
-import { CustomBaseEntity } from "./BaseEntity"
+import { CustomBaseEntity } from './BaseEntity'
 
 /**
- * Default data for the Data table (dynamic EAV key/value pattern) 
+ * Default data for the Data table (dynamic EAV key/value pattern)
  */
- export const defaultData = {
-
+export const defaultData = {
     maintenance: false,
     lastMaintenance: Date.now(),
-    lastStartup: Date.now()
+    lastStartup: Date.now(),
 }
 
 type DataType = keyof typeof defaultData
@@ -21,11 +26,10 @@ type DataType = keyof typeof defaultData
 
 @Entity({ customRepository: () => DataRepository })
 export class Data extends CustomBaseEntity {
-
     [EntityRepositoryType]?: DataRepository
 
     @PrimaryKey()
-    key!: string 
+    key!: string
 
     @Property()
     value: string = ''
@@ -36,43 +40,74 @@ export class Data extends CustomBaseEntity {
 // ===========================================
 
 export class DataRepository extends EntityRepository<Data> {
-
     async get<T extends DataType>(key: T): Promise<typeof defaultData[T]> {
-
         const data = await this.findOne({ key })
 
         return JSON.parse(data!.value)
     }
 
-    async set<T extends DataType>(key: T, value: typeof defaultData[T]): Promise<void> {
-
+    async set<T extends DataType>(
+        key: T,
+        value: typeof defaultData[T]
+    ): Promise<void> {
         const data = await this.findOne({ key })
 
         if (!data) {
-
             const newData = new Data()
             newData.key = key
             newData.value = JSON.stringify(value)
 
             await this.persistAndFlush(newData)
-        }
-        else {
+        } else {
             data.value = JSON.stringify(value)
             await this.flush()
         }
     }
 
-    async add<T extends DataType>(key: T, value: typeof defaultData[T]): Promise<void> {
-
+    async add<T extends DataType>(
+        key: T,
+        value: typeof defaultData[T]
+    ): Promise<void> {
         const data = await this.findOne({ key })
 
         if (!data) {
-
             const newData = new Data()
             newData.key = key
             newData.value = JSON.stringify(value)
 
             await this.persistAndFlush(newData)
         }
+    }
+}
+
+export class ModelData {
+    async get<T extends DataType>(key: T): Promise<typeof defaultData[T]> {
+        const data = await prisma.data.findUnique({
+            where: {
+                key: key,
+            },
+        })
+
+        if (!data) return JSON.parse('false')
+
+        return JSON.parse(data.value!)
+    }
+
+    async set<T extends DataType>(
+        key: T,
+        value: typeof defaultData[T]
+    ): Promise<void> {
+        await prisma.data.upsert({
+            create: {
+                key,
+                value: JSON.stringify(value),
+            },
+            update: {
+                value: JSON.stringify(value),
+            },
+            where: {
+                key,
+            },
+        })
     }
 }
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,7 +2,7 @@
 
 export * from './User'
 export * from './Guild'
-export { Data } from './Data'
+export { Data, ModelData } from './Data'
 export * from './Stat'
 export * from './Image'
 export * from './Pastebin'
